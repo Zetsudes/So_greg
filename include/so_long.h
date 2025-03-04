3c111747ed47baae4c5341a085f9e2676a7d7e9f
Zainab Mohamed <zamohame@c2r4p9.42nice.fr>
2025-03-04 17:37:36 +0100

    h

--- include/so_long.h
+++ include/so_long.h
@@ -6,13 +6,20 @@
 /*   By: zamohame <zamohame@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 20:48:51 by zamohame          #+#    #+#             */
-/*   Updated: 2025/02/25 20:52:09 by zamohame         ###   ########.fr       */
+/*   Updated: 2025/03/03 11:13:01 by zamohame         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
-# define SO_LONG_H 
+# define SO_LONG_H
 
 # include "../greg_lib/libft.h"
+
+typedef struct s_count
+{
+	int	p;
+	int	c;
+	int	e;
+}		t_count;
 
 #endif
--- src/parsing.c
+++ src/parsing.c
@@ -1,6 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zamohame <zamohame@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/03 11:13:09 by zamohame          #+#    #+#             */
+/*   Updated: 2025/03/03 11:27:00 by zamohame         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../include/so_long.h"
 
+void	init_count(t_count *count)
+{
+	count->p = 0;
+	count->c = 0;
+	count->e = 0;
+}
+
 void	check_map_elements(char **map)
+{
+	int		i;
+	int		j;
+	t_count	count;
+
+	init_count(&count);
+	i = 0;
+	while (map[i])
+	{
+		j = 0;
+		while (map[i][j])
+		{
+			if (map[i][j] != '0' && map[i][j] != '1' && map[i][j] != 'C'
+				&& map[i][j] != 'E' && map[i][j] != 'P')
+			{
+				ft_printf("Error: Invalid character '%c' at row %d column %d\n",
+					map[i][j], i, j);
+				exit(1);
+			}
+			j++;
+		}
+		i++;
+	}
+	count_elements(map, &count);
+	check_elements_count(count);
+}
+
+void	count_elements(char **map, t_count *count)
 {
 	int	i;
 	int	j;
@@ -11,21 +58,35 @@ void	check_map_elements(char **map)
 		j = 0;
 		while (map[i][j])
 		{
-			if (map[i][j] != '0' && map[i][j] != '1' && map[i][j] != 'C'
-				&& map[i][j] != 'E' && map[i][j] != 'P')
-			{
-				ft_printf("Error :\n Invalid character '%c' at row %d, column
-					%d", map[i][j], i, j);
-				exit(1);
-			}
+			if (map[i][j] == 'P')
+				count->p++;
+			if (map[i][j] == 'C')
+				count->c++;
+			if (map[i][j] == 'E')
+				count->e++;
 			j++;
 		}
 		i++;
 	}
 }
 
-void	check_elements_count(char **map)
+void	check_elements_count(t_count count)
 {
+	if (count.p != 1)
+	{
+		printf("Error: Map must contain exactly one player P\n");
+		exit(1);
+	}
+	if (count.c < 1)
+	{
+		printf("Error: Map must contain at least one collectible C\n");
+		exit(1);
+	}
+	if (count.e != 1)
+	{
+		printf("Error: Map must contain exactly one exit E\n");
+		exit(1);
+	}
 }
 
 void	check_map_walls(char **map)
